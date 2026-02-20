d668be7fcbb92c6a1af1ba0fd38b251b82a3ec7d
ibntofajjal <joynal.bintofajjal@gmail.com>
2026-02-21 01:49:10 +0600

    Add authentication middleware and extend Express Request type
    - Implemented auth middleware to verify JWT and check user roles and status.
    - Added error handling for unauthorized access and banned users.
    - Extended Express Request type to include user information from JWT payload.

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { STATUS, USER_ROLE } from "../../prisma/generated/prisma/enums";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { prisma } from "../lib/prisma";
+
+export const auth = (...roles: USER_ROLE[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) throw new Error("Token Not Found");
+      const decodedUser = jwt.verify(token, config.jwt_secret!) as JwtPayload;
+
+      // Check the db, that the provided user is in the db or just a fake req
+      const userData = await prisma.user.findUnique({
+        select: { email: true, role: true, status: true },
+        where: {
+          email: decodedUser.email,
+        },
+      });
+      if (!userData) throw new Error("Unauthorized");
+
+      // is the given role include in the token or not, if so then should allow for the req if not throw the error.
+      if (userData.status === STATUS.BAN) {
+        throw new Error("You are Banned");
+      } else if (roles.length && !roles.includes(decodedUser.role)) {
+        throw new Error("Unauthorized");
+      }
+
+      next();
+    } catch (error: any) {
+      next(error);
+    }
+  };
+};
